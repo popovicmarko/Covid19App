89f9a07b44803092f5739553d1289fb9867ef1a4
popovicmarko <popovicmarko350@gmail.com>
2023-06-22 19:20:10 +0200

    updete jsen file

--- Covid19StatisticApp/src/pages/Team/Team.jsx
+++ Covid19StatisticApp/src/pages/Team/Team.jsx
@@ -1,13 +1,14 @@
 import NavBar from "../../components/NavBar/NavBar";
 import persons from "../../common/persons.json"
 import PersonalCard from "../../components/Cards/PersonalCard";
+import "./Team.css"
 
 
 function Team () {
     return (
         <div>
             <NavBar />
-            <div>
+            <div className="wrap">
                 {
                     persons.map(e=> <PersonalCard key={e.name} name={e.name} fullName={e.fullName} occupation={e.occupation} description={e.description} gitHubLink={e.gitHubLink}/>)
                 }
